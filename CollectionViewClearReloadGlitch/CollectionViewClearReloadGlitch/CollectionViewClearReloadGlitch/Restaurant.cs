f653d8baca28f12b12d5b598bf87f1e098f5e0bd
Ryan Payne <ryan.payne@daveramsey.com>
2022-01-03 14:04:55 -0600

    Simplify Restaurant model

--- CollectionViewClearReloadGlitch/CollectionViewClearReloadGlitch/CollectionViewClearReloadGlitch/Restaurant.cs
+++ CollectionViewClearReloadGlitch/CollectionViewClearReloadGlitch/CollectionViewClearReloadGlitch/Restaurant.cs
@@ -4,40 +4,29 @@ namespace CollectionViewClearReloadGlitch
     {
         public string Name { get; }
         private int Rating { get; }
-        public string Stars { get; private set; }
+
+        public string Stars
+        {
+            get
+            {
+                return Rating switch
+                {
+                    0 => "",
+                    1 => "⭐️",
+                    2 => "⭐️⭐️",
+                    3 => "⭐️⭐️⭐️",
+                    4 => "⭐️⭐️⭐️⭐️",
+                    5 => "⭐️⭐️⭐️⭐️⭐️",
+                    _ => ""
+                };
+            }
+        }
         public bool StarsAreVisible => Rating > 0;
 
         public Restaurant(string name, int rating = 0)
         {
             Name = name;
             Rating = rating;
-
-            SetStars();
-        }
-
-        private void SetStars()
-        {
-            switch (Rating)
-            {
-                case 0:
-                    Stars = "";
-                    break;
-                case 1:
-                    Stars = "⭐️";
-                    break;
-                case 2:
-                    Stars = "⭐️⭐️";
-                    break;
-                case 3:
-                    Stars = "⭐️⭐️⭐️";
-                    break;
-                case 4:
-                    Stars = "⭐️⭐️⭐️⭐️";
-                    break;
-                case 5:
-                    Stars = "⭐️⭐️⭐️⭐️⭐️";
-                    break;
-            }
         }
     }
 }
